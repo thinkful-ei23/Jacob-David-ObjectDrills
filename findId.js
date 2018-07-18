1d4739f96ff0f9494d076a4cff14707373f2ffb9
David Graves <davidgraves831@gmail.com>
2018-07-18 13:53:44 -0500

    finished findId.js

---
+++ findId.js
@@ -0,0 +1,11 @@
+const scratchData = [
+    { id: 22, foo: 'bar' },
+    { id: 28, foo: 'bizz' },
+    { id: 19, foo: 'bazz' },
+  ];
+  
+  function findById(items, idNum) {
+    return items.find(item => item.id === idNum);
+  }
+
+  
