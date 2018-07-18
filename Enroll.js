0d031f983df9b7380f0dceac464b06100dd7bab5
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-07-18 14:47:39 -0400

    completed Enroll.js

---
+++ Enroll.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const studentData = [
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology',
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics',
+  },
+  {
+    name: 'Liz',
+    status: 'On leave',
+    course: 'Computer science',
+  },
+];
+
+function enrollInSummerSchool(students) {
+  return students.map(stud => {
+    return {
+      name: stud.name,
+      status: 'In Summer school',
+      course: stud.course
+    };
+  });
+}
